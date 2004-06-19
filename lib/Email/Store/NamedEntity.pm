af8fedf3d85cb334604dd0399b9013a0fa78cde8
Simon Wistow <simonw@cpan.org>
2004-06-19 17:23:29 +0000

    Import of SIMONW/Email-Store-NamedEntity-1.01 from CPAN.
    gitpan-cpan-distribution: Email-Store-NamedEntity
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         SIMONW/Email-Store-NamedEntity-1.01.tar.gz
    gitpan-cpan-author:       SIMONW
    gitpan-cpan-maturity:     released

--- lib/Email/Store/NamedEntity.pm
+++ lib/Email/Store/NamedEntity.pm
@@ -2,7 +2,7 @@ package Email::Store::NamedEntity;
 use 5.006;
 use strict;
 use warnings;
-our $VERSION = '1.0';
+our $VERSION = '1.01';
 use Email::Store::DBI;
 use base 'Email::Store::DBI';
 use Email::Store::Mail;
@@ -45,7 +45,7 @@ sub on_gather_plucene_fields {
 
     my %topics;
     foreach my $e ($mail->named_entities) {
-        push @{$topic{lc($e->description)}, lc($e->thing);
+        push @{$topics{lc($e->description)}, lc($e->thing);
     }
 
     foreach my $key (keys %topics) {
